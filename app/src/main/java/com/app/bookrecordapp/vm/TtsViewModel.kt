5f9bd9eec005c9154a7309fd8cd1ac2f586b28e8
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2023-08-23 05:41:41 +0900

    0823_AM 05:41

--- app/src/main/java/com/app/bookrecordapp/vm/TtsViewModel.kt
+++ app/src/main/java/com/app/bookrecordapp/vm/TtsViewModel.kt
@@ -2,47 +2,38 @@ package com.app.bookrecordapp.vm
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
-import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.app.bookrecordapp.screen.TtsScreenState
 import java.util.Locale
 
-class TtsViewModel: ViewModel() {
+class TtsViewModel : ViewModel() {
 
-    private val _state = mutableStateOf(TtsScreenState())
-    val state: State<TtsScreenState> = _state
-    private  var  textToSpeech: TextToSpeech? = null
+    val state = mutableStateOf(TtsScreenState())
+    private var textToSpeech: TextToSpeech? = null
 
-
-    fun onTextFieldValueChange(text:String){
-        _state.value = state.value.copy(
-            text = text
-        )
+    fun onTextFieldValueChange(text: String) {
+        state.value = state.value.copy(text = text)
     }
 
-    fun textToSpeech(context: Context){
-        _state.value = state.value.copy(
-            isButtonEnabled = false
-        )
-        textToSpeech = TextToSpeech(
-            context
-        ) {
-            if (it == TextToSpeech.SUCCESS) {
-                textToSpeech?.let { txtToSpeech ->
-                    txtToSpeech.language = Locale.KOREAN
-                    txtToSpeech.setSpeechRate(1.0f)
-                    txtToSpeech.speak(
-                        _state.value.text,
-                        TextToSpeech.QUEUE_ADD,
-                        null,
-                        null
-                    )
+    fun textToSpeech(context: Context) {
+        state.value = state.value.copy(isButtonEnabled = false)
+
+        textToSpeech = TextToSpeech(context) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                textToSpeech?.apply {
+                    language = Locale.KOREAN
+                    setSpeechRate(1.0f)
+                    speak(state.value.text, TextToSpeech.QUEUE_ADD, null, null)
                 }
             }
-            _state.value = state.value.copy(
-                isButtonEnabled = true
-            )
+            state.value = state.value.copy(isButtonEnabled = true)
         }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        textToSpeech?.stop()
+        textToSpeech?.shutdown()
     }
 }
